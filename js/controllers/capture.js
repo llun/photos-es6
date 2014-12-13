2e0fd01146f072146828a6671ebe2ad891709985
Maythee Anegboonlap <null@llun.in.th>
2014-12-13 23:10:22 +0800

    Add all empty actions and flags.

--- js/controllers/capture.js
+++ js/controllers/capture.js
@@ -1,3 +1,30 @@
 app.controller('CaptureCtrl', ['$scope',
   function($scope) {
+    $scope.resultClasses = "hidden"
+    $scope.isFilterActionsDisabled = true
+    $scope.isActionsDisabled = true
+
+    $scope.filters = [
+      { name: 'vintage', label: 'Vintage' },
+      { name: 'lomo', label: 'Lomo' },
+      { name: 'clarity', label: 'Clarity' },
+      { name: 'sinCity', label: 'Sin City' },
+      { name: 'sunrise', label: 'Sunrise' },
+      { name: 'crossProcess', label: 'Cross Process' },
+      { name: 'orangePeel', label: 'Orange Peel' },
+      { name: 'love', label: 'Love' },
+      { name: 'grungy', label: 'Grungy' }
+    ]
+
+    $scope.updateCanvas = function() {
+      console.log ("Updating canvas")
+    }
+
+    $scope.applyFilter = function(filter) {
+      console.log(filter)
+    }
+
+    $scope.resetFilter = function() {
+
+    }
   }])
